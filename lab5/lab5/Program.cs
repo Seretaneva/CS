9e08f3ace09a59364bf9032e4c49fb12474d50b7
Seretaneva <seretan30@gmail.com>
2020-11-27 11:53:29 +0200

    Commit the lab

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http:www.youtube.com");
+            driver.Manage().Window.Maximize();
+            object p = driver.Manage().Timeouts().ImplicitWait;
+            driver.FindElement(By.Name("search_query")).Click();
+            driver.FindElement(By.Name("search_query")).SendKeys("computer");
+            driver.FindElement(By.Id("search-icon-legacy")).Click();
+            bool result = driver.FindElement(By.Id("logo-icon-container")).Displayed;
+            Console.WriteLine($"The result is displayed!!! + {result}");
+        }
+    }
+}
